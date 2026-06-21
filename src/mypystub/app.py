323d575c86a9d982c7eb145f739a50101536ae61
Joshua King <jkingok@users.noreply.github.com>
2026-06-21 08:36:58 +0800

    Tutorial step 2

--- src/mypystub/app.py
+++ src/mypystub/app.py
@@ -8,18 +8,33 @@ from toga.style.pack import COLUMN, ROW
 
 class MyPyStub(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
+        main_box = toga.Box(direction=COLUMN)
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        name_label = toga.Label(
+            "Your name: ",
+            margin=(0, 5),
+        )
+        self.name_input = toga.TextInput(flex=1)
+
+        name_box = toga.Box(direction=ROW, margin=5)
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+
+        button = toga.Button(
+            "Say Hello!",
+            on_press=self.say_hello,
+            margin=5,
+        )
+
+        main_box.add(name_box)
+        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
+    def say_hello(self, widget):
+        print(f"Hello, {self.name_input.value}")
 
 def main():
     return MyPyStub()
