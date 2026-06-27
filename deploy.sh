2fdebeb2d2cc1374b4225c43f37cf912603d802e
Joshua King <jkingok@users.noreply.github.com>
2026-06-27 16:49:16 +0800

    Add deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
-set -e
+#set -e
 
 export SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-source "${SCRIPT_DIR}/env"
-
 # Configuration Definitions
-TARGET_UDID="0FD48B24-D64E-5EBD-8F16-58A7D14A4CFD"
 BUNDLE_ID="au.id.jking.mypystub"
-PROJECT_ROOT="/Users/joshua/beeware-tutorial/mypystub"
+PROJECT_NAME="MyPyStub"
+PROJECT_ROOT="$SCRIPT_DIR"
 
 cd "$PROJECT_ROOT"
 
 echo "🔄 Syncing current Python code variations..."
-briefcase update ios
+briefcase update ios || exit 1
 
 echo "🏗 Moving to Xcode Workspace..."
 cd build/mypystub/ios/xcode/
 
 echo "🔨 Executing native xcodebuild compilation for physical hardware..."
-xcodebuild -project "MyPyStub.xcodeproj" \
-           -scheme "MyPyStub" \
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" \
+           -scheme "${PROJECT_NAME}" \
            -configuration Debug \
            -destination "generic/platform=iOS" \
            -sdk iphoneos \
            ONLY_ACTIVE_ARCH=NO \
            SYMROOT="$PWD/build" \
-           clean build
+           clean build || exit 2
 
-echo "📲 Moving binary over local network..."
-xcrun devicectl device install app --device "$TARGET_UDID" "build/Debug-iphoneos/MyPyStub.app"
+for DEVICE in $(xcrun devicectl list devices -j - | jq -r '.result.devices[] | .identifier'); do
+	echo "📲 Moving binary over local network..."
+	xcrun devicectl device install app --device "$DEVICE" "build/Debug-iphoneos/MyPyStub.app"
 
-echo "🚀 Triggering application launch loop..."
-xcrun devicectl device process launch --device "$TARGET_UDID" "$BUNDLE_ID"
-
-echo "✔ Application updated and running smoothly!"
+	#echo "🚀 Triggering application launch loop..."
+	#xcrun devicectl device process launch --device "$DEVICE" "$BUNDLE_ID"
+done
